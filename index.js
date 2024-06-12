c97a24bb40f1173081a756008905882b9acb7c44
Fardus-Hassan <webdev.fardus@gmail.com>
2024-06-12 18:07:37 +0600

    post user info

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+      ],
+      credentials: true,
+    })
+  );
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ak5lvkp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+    const UserCollection = client.db("EmployeeFlowDB").collection("UserCollection");
+
+
+
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const result = await UserCollection.insertOne(user);
+        res.json(result);
+    })
+
+    app.get('/users', async (req, res) => {
+        const users = await UserCollection.find({}).toArray();
+        res.json(users);
+    })
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('This is EmployeeFlow-server')
+})
+
+app.listen(port, () => {
+    console.log(`EmployeeFlow-server running on port ${port}`)
+})
